bc18ffe8912dd373dd77f933ec2d58e5179e57b0
RobTain <robsoft.softwaredevelopment@gmail.com>
2018-02-17 00:26:45 +0100

    Final Push Part1

--- src/main/resources/data/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO User(FORENAME, SURNAME, POSITION, LINK) values ('Max', 'Mustermann', 'Janitor', 'https://github.com/RobTain');
-INSERT INTO User(FORENAME, SURNAME, POSITION, LINK) values ('Peter', 'Mustermann', 'Janitor', 'https://github.com/RobTain1');
-INSERT INTO User(FORENAME, SURNAME, POSITION, LINK) values ('Robert', 'Mustermann', 'Janitor', 'https://github.com/RobTain2');
-INSERT INTO User(FORENAME, SURNAME, POSITION, LINK) values ('Marcel', 'Mustermann', 'Janitor', 'https://github.com/RobTain3');
-INSERT INTO User(FORENAME, SURNAME, POSITION, LINK) values ('Michael', 'Mustermann', 'Janitor', 'https://github.com/RobTain4');
