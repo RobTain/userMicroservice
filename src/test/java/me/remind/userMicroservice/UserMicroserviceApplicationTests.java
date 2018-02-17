29845539d11974867216a69d88f3a5508938fc7e
RobTain <robsoft.softwaredevelopment@gmail.com>
2018-02-17 02:29:49 +0100

    minor change

--- src/test/java/me/remind/userMicroservice/UserMicroserviceApplicationTests.java
+++ src/test/java/me/remind/userMicroservice/UserMicroserviceApplicationTests.java
@@ -2,15 +2,33 @@ package me.remind.userMicroservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import me.remind.userMicroservice.controller.UserController;
+import me.remind.userMicroservice.model.User;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(UserController.class)
 public class UserMicroserviceApplicationTests {
 
+	@MockBean
+	private UserController userController;
+	
+	
 	@Test
-	public void contextLoads() {
-	}
+	public void createValidUser() {
+		User user = new User();
+		user.setForename("Robert");
+		user.setSurname("Koenig");
+		user.setPosition("Java Developer");
+		user.setLink("https://github.com/RobTain");
+		
+		userController.createUser(user);		
+		
+	 }
+
+
 
 }
