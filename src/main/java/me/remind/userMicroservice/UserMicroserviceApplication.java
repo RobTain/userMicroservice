a0d3270784495d5eaf66afafc44fd553c386deaa
RobTain <robsoft.softwaredevelopment@gmail.com>
2018-03-02 10:04:19 +0100

    Add TODOS

--- src/main/java/me/remind/userMicroservice/UserMicroserviceApplication.java
+++ src/main/java/me/remind/userMicroservice/UserMicroserviceApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserMicroserviceApplication {
 
+/*
+ * TODO: Github Service bauen -> Für jede Entity mit Aufgabe gibt es einen eigenen Service (mit Auth2)
+ * TODO: Service benötigt keine SQL Anfragen - baut Spring automatisch! Nachschauen!!!
+ * TODO: Tests nicht nach Reihenfolge, sondern abgeschlossen 
+ */
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserMicroserviceApplication.class, args);
